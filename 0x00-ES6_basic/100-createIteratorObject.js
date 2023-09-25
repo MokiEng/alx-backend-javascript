c09668cae9fda7fad8b54b936b1f8b41befb7d94
MokiEng <meconen20@gmail.com>
2023-09-25 20:01:19 +0000

    iterator and iterable

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,8 +0,0 @@
-export default function* createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  for (const employees of departments) {
-    for (const employee of employees) {
-      yield employee;
-    }
-  }
-}
