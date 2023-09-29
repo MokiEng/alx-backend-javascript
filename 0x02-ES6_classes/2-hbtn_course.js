bd1db519b5dc5188faf499481138d8d61673d418
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-09-29 06:13:12 -0700

    getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,30 +4,37 @@ export default class HolbertonCourse {
     this._length = this.validateNumber(length, 'Length');
     this._students = this.validateStudents(students);
   }
+
   // Getter for name
   get name() {
     return this._name;
   }
+
   // Setter for name
   set name(value) {
     this._name = this.validateString(value, 'Name');
   }
+
   // Getter for length
   get length() {
     return this._length;
   }
+
   // Setter for length
   set length(value) {
     this._length = this.validateNumber(value, 'Length');
   }
+
   // Getter for students
   get students() {
     return this._students;
   }
+
   // Setter for students
   set students(value) {
     this._students = this.validateStudents(value);
   }
+
   // Helper method to validate a string attribute
   validateString(value, attributeName) {
     if (typeof value !== 'string') {
@@ -35,6 +42,7 @@ export default class HolbertonCourse {
     }
     return value;
   }
+
   // Helper method to validate a number attribute
   validateNumber(value, attributeName) {
     if (typeof value !== 'number') {
@@ -42,6 +50,7 @@ export default class HolbertonCourse {
     }
     return value;
   }
+
   // Helper method to validate the students attribute
   validateStudents(value) {
     if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
