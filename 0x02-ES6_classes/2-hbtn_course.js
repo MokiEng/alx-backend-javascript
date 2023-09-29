13ac7977272e8fbabf9b2bbb536c79e61bec00d7
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-09-29 14:12:32 -0700

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,61 +1,67 @@
 export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'Name');
-    this._length = this.validateNumber(length, 'Length');
-    this._students = this.validateStudents(students);
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter for name
+  /**
+   * Gets the name of this course.
+   */
   get name() {
     return this._name;
   }
 
-  // Setter for name
+  /**
+   * Sets the name of this course.
+   */
   set name(value) {
-    this._name = this.validateString(value, 'Name');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
   }
 
-  // Getter for length
+  /**
+   * Gets the length of this course (in months).
+   */
   get length() {
     return this._length;
   }
 
-  // Setter for length
+  /**
+   * Sets the length of this course (in months).
+   */
   set length(value) {
-    this._length = this.validateNumber(value, 'Length');
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
   }
 
-  // Getter for students
+  /**
+   * Gets the names of students in this course.
+   */
   get students() {
     return this._students;
   }
 
-  // Setter for students
+  /**
+   * Sets the names of students in this course.
+   */
   set students(value) {
-    this._students = this.validateStudents(value);
-  }
-
-  // Helper method to validate a string attribute
-  validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
-    }
-    return value;
-  }
-
-  // Helper method to validate a number attribute
-  validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  // Helper method to validate the students attribute
-  validateStudents(value) {
-    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+    if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
-    return value;
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
