768c2ce25fa9012aa193bc7ee7d1afc996655662
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-09-30 11:14:06 -0700

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  let newTask = task;
-  let newTask2 = task2;
-
   if (trueOrFalse) {
-    newTask = true;
-    newTask2 = false;
+    const task = true;
+    const task2 = false;
   }
 
-  return [newTask, newTask2];
+  return [task, task2];
 }
