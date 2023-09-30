fb7ab9fcd6d73d0a1678a2adab761f18c177e484
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-09-30 11:21:15 -0700

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,6 @@
+/**
+ * @returns {Promise<string>} A Promise that resolves to the API response data.
+ */
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
