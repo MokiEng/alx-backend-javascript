9c0477ac6ce176fb03d137ac618563f441268af1
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-21 09:34:15 -0800

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import routes from './routes';
+import mapRoutes from './routes';
 
 const app = express();
+const PORT = 1245;
 
-app.use('/', routes);
-
-const PORT = process.env.PORT || 1245;
-
+mapRoutes(app);
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 export default app;
+module.exports = app;
