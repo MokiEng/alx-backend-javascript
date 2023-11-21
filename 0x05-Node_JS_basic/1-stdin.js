6f3d8971c75a3101ab2760026ca40179ba0fc078
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-21 09:01:17 -0800

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-process.stdin.resume();
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name !== '') {
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
