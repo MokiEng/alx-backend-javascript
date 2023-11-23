71bf7f86d0cedbf6653cc99ed253fca854fad394
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-23 06:13:12 -0800

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,33 +1,14 @@
 const request = require('request');
 const { expect } = require('chai');
 
-const app = require('./api');
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', () => {
-  before((done) => {
-    app.listen(7865, () => {
-      console.log('API available on localhost port 7865');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
-
-  it('Correct status code?', (done) => {
-    request.get('http://localhost:7865/', (error, response) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('Correct result?', (done) => {
-    request.get('http://localhost:7865/', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  // Add other tests as needed
-
-  after(() => {
-    app.close();
-  });
-});
+}
